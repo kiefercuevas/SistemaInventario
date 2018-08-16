6ae1cdccdafca2d7307a86232ed4c065ab47ed20
kiefercuevas <kiefercg2014@gmail.com>
2018-08-16 02:59:10 -0400

    se elimino el controlador de transactions , se agrego el view, modelview y controller de ventas

---
+++ DgrosStore/Controllers/SalesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DgrosStore.Models.viewModels;
+using DgrosStore.Models;
+namespace DgrosStore.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly DgrosStoreContext dgrosStore;
+        public SalesController()
+        {
+            dgrosStore = new DgrosStoreContext();
+        }
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var Sales = new SalesViewModel()
+            {
+                Clients = dgrosStore.Clients.ToList(),
+                Products = dgrosStore.Products.ToList(),
+                Sales = new Sales(),
+                salesProducs = new SalesProducs()
+                
+            };
+            return View("SalesIndex",Sales);
+        }
+    }
+}
---
+++ DgrosStore/Models/viewModels/SalesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DgrosStore.Models.viewModels
+{
+    public class SalesViewModel
+    {
+        public IEnumerable<Client> Clients;
+        public Client SelectedClient;
+
+        public IEnumerable<Product> Products;
+        public Product selectedProduct;
+
+        public Sales Sales;
+        public SalesProducs salesProducs;
+
+    }
+}
