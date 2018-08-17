22e9565565dc8d989e1d279d28aa7e88be74fa2c
kiefercuevas <kiefercg2014@gmail.com>
2018-08-17 14:51:36 -0400

    se modifico el apartado de ventas agregando ajax para la carga de clientes y productos, se modifico el enum de metodo de pago para que despliegue los nombres en español

--- DgrosStore/Controllers/SalesController.cs
+++ DgrosStore/Controllers/SalesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using DgrosStore.Models.viewModels;
 using DgrosStore.Models;
+using System.Web.Script.Serialization;
 namespace DgrosStore.Controllers
 {
     public class SalesController : Controller
@@ -26,6 +27,47 @@ namespace DgrosStore.Controllers
                 
             };
             return View("SalesIndex",Sales);
+        }
+
+        [Route("Sales/GetClients")]
+        public ActionResult GetClients(string client)
+        {
+
+            var jsonSerialiser = new JavaScriptSerializer();
+            var clientInDB = dgrosStore.Clients
+                .Where(c => c.Name.IndexOf(client) > -1)
+                .Select(c => new
+                {
+                    id = c.ClientId,
+                    name = c.Name,
+                    lastName = c.LastName
+                }).ToList();
+
+
+            var JsonClients = jsonSerialiser
+                .Serialize(clientInDB);
+
+            return Json(JsonClients, JsonRequestBehavior.AllowGet);
+        }
+
+        [Route("Sales/GetProducts")]
+        public ActionResult GetProducts(string product)
+        {
+
+            var jsonSerialiser = new JavaScriptSerializer();
+            var productInDB = dgrosStore.Products
+                .Where(p => p.Name.IndexOf(product) > -1)
+                .Select(p => new
+                {
+                    id = p.ProductId,
+                    name = p.Name,
+                }).ToList();
+
+
+            var JsonProduct = jsonSerialiser
+                .Serialize(productInDB);
+
+            return Json(JsonProduct, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- DgrosStore/Models/paymentMethod.cs
+++ DgrosStore/Models/paymentMethod.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace DgrosStore.Models
 {
     public enum PaymentMethod
     {
+        [Display(Name ="Dinero en efectivo")]
         Money=1,
+        [Display(Name = "Transferencia bancaria")]
         WireTransfer,
+        [Display(Name = "Pago con tarjeta")]
         CardPayment
     }
 }
